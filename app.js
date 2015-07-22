c995497e55d1c79160bd9e59ea213b8edad0fcd2
Prashant Baid <prashant@Prashants-MacBook-Air.local>
2015-07-22 23:54:24 +0530

    fixing indentation issues

--- app.js
+++ app.js
@@ -2,61 +2,61 @@ var loopback = require('loopback');
 
 function loopbackApp() {
 
-	var app = loopback();
+    var app = loopback();
 
-	//datasource configuration
-	var dsConfig = {
-		dsName : 'db',
-		options : {'connector': 'memory'}
-	}
+    //datasource configuration
+    var dsConfig = {
+        dsName : 'db',
+        options : {'connector': 'memory'}
+    }
 
-	//model configuration
-	var modelConfig = {
-		modelName : 'Movies',
-		options : {
-			"name": "Movies",
-		    "plural": "Movies",
-		    "base": "Model",
-		    "strict":"validate",
-		    "idInjection": false,
-		    "properties": {
-		      "name": {
-		        "type": "string",
-		        "required": true
-		      },
-		      "genre": {
-		        "type": "string",
-		        "required": true
-		      },
-		      "year": {
-		        "type": "number",
-		        "required": true
-		      }
-		    },
-		    "dataSource": "db",
-		    "public": true
-		}
-	}
+    //model configuration
+    var modelConfig = {
+        modelName : 'Movies',
+        options : {
+            "name": "Movies",
+            "plural": "Movies",
+            "base": "Model",
+            "strict":"validate",
+            "idInjection": false,
+            "properties": {
+              "name": {
+                "type": "string",
+                "required": true
+              },
+              "genre": {
+                "type": "string",
+                "required": true
+              },
+              "year": {
+                "type": "number",
+                "required": true
+              }
+            },
+            "dataSource": "db",
+            "public": true
+        }
+    }
 
-	//app configuration
-	var config = {
-		"restApiRoot" : "/api",
-		"port" : 3005
-	}
+    //app configuration
+    var config = {
+        "restApiRoot" : "/api",
+        "port" : 3005
+    }
 
-	var myModel;
+    var myModel;
 
 
-	//load everything
-	loadDataSource(app, dsConfig);
-	myModel = loadModel(app, modelConfig);
-	loadRemoteMethods(myModel);
-	loadSampleData(myModel);
-	loadExplorer(app, config);
-	loadMiddlewares(app, config);
+    //load everything
+    loadDataSource(app, dsConfig);
+    myModel = loadModel(app, modelConfig);
+    loadRemoteMethods(myModel);
+    loadSampleData(myModel);
+    loadExplorer(app, config);
+    loadMiddlewares(app, config);
 
-	//start server
-	startServer(app, config);
+    //start server
+    startServer(app, config);
 
 }
 
@@ -64,19 +64,19 @@ loopbackApp();
 
 
 function loadDataSource(app, dsConfig) {
-	app.dataSource(dsConfig.dsName, dsConfig.options);	
+    app.dataSource(dsConfig.dsName, dsConfig.options);  
 }
 
 
 function loadModel(app, modelConfig) {
-	app.model(modelConfig.modelName, modelConfig.options);
-	return app.models[modelConfig.modelName];
+    app.model(modelConfig.modelName, modelConfig.options);
+    return app.models[modelConfig.modelName];
 }
 
 
 function loadRemoteMethods(myModel) {
-	
-	myModel.findByGenre = function(genre, next) {
+    
+    myModel.findByGenre = function(genre, next) {
         var clause = {where: {genre: genre}};
         myModel.find(clause, function(err, data) {
            next(err, data);
@@ -111,7 +111,7 @@ function loadRemoteMethods(myModel) {
 
 
 function loadSampleData(myModel) {
-	myModel.create([
+    myModel.create([
       {name: 'Andaz Apna Apna', genre: 'Comedy', year: 1994},
       {name: 'The Grand Budapest Hotel', genre: 'Adventure', year: 2014},
       {name: '12 Angry Men', genre: 'Crime', year: 1957},
@@ -123,45 +123,45 @@ function loadSampleData(myModel) {
 
 
 function loadExplorer(app, config) {
-	var explorer;
-	
-	try {
-		explorer = require('loopback-explorer');
-	} catch(err) {
-		console.log(
-		  'Run `npm install loopback-explorer` to enable the LoopBack explorer'
-		);
-		return;
-	}
+    var explorer;
+    
+    try {
+        explorer = require('loopback-explorer');
+    } catch(err) {
+        console.log(
+          'Run `npm install loopback-explorer` to enable the LoopBack explorer'
+        );
+        return;
+    }
 
-	var restApiRoot = config['restApiRoot'];
+    var restApiRoot = config['restApiRoot'];
 
-	var explorerApp = explorer(app, { basePath: restApiRoot });
-	app.use('/explorer', explorerApp);
+    var explorerApp = explorer(app, { basePath: restApiRoot });
+    app.use('/explorer', explorerApp);
 
-	app.once('started', function() {
-		var explorerPath = explorerApp.mountpath || explorerApp.route;
-	});
+    app.once('started', function() {
+        var explorerPath = explorerApp.mountpath || explorerApp.route;
+    });
 }
 
 
 function loadMiddlewares(app, config) {
-	app.use(config['restApiRoot'],loopback.rest());
+    app.use(config['restApiRoot'],loopback.rest());
 }
 
-
+ 
 function startServer(app, config) {
-	app.start = function() {
-	  // start the web server
-	  return app.listen(function() {
-	    app.emit('started');
-	    console.log('Web server started');
-	  });
-	};
+    app.start = function() {
+      // start the web server
+      return app.listen(function() {
+        app.emit('started');
+        console.log('Web server started');
+      });
+    };
 
-	if (require.main === module) {
-	  app.start();
-	}
+    if (require.main === module) {
+      app.start();
+    }
 
-	app.listen(config['port']);	
+    app.listen(config['port']); 
 }
